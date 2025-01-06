cf38351ae3afc0bc5b6ca486008af2c44a1dafc5
Alexey Trukhlyayev <exact84@gmail.com>
2025-01-06 19:30:45 +0500

    feat: all tasks done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -22,20 +22,43 @@ const { NotImplementedError } = require("../extensions/index.js");
 class VigenereCipheringMachine {
   constructor(param = true) {
     this.param = param;
+    this.code = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
   encrypt(message, key) {
     if (!message || !key) throw new Error("Incorrect arguments!");
-    message = message.toUpperCase();
-    key = key.toUpperCase();
-
-    return "";
+    const messageU = message.toUpperCase();
+    const keyU = key.toUpperCase();
+    let result = "";
+    let j = 0;
+    for (let i = 0; i < messageU.length; i += 1) {
+      const charIndex = this.code.indexOf(messageU[i]);
+      if (charIndex !== -1) {
+        const keyIndex = this.code.indexOf(keyU[j % keyU.length]);
+        const shiftedIndex = (charIndex + keyIndex) % 26;
+        result += this.code[shiftedIndex];
+        j += 1;
+      } else result += messageU[i];
+    }
+    result = this.param ? result : result.split("").reverse().join("");
+    return result;
   }
   decrypt(message, key) {
     if (!message || !key) throw new Error("Incorrect arguments!");
-    message = message.toUpperCase();
-    key = key.toUpperCase();
-
-    return "";
+    const messageU = message.toUpperCase();
+    const keyU = key.toUpperCase();
+    let result = "";
+    let j = 0;
+    for (let i = 0; i < messageU.length; i += 1) {
+      const charIndex = this.code.indexOf(messageU[i]);
+      if (charIndex !== -1) {
+        const keyIndex = this.code.indexOf(keyU[j % keyU.length]);
+        const shiftedIndex = (charIndex - keyIndex + 26) % 26;
+        result += this.code[shiftedIndex];
+        j += 1;
+      } else result += messageU[i];
+    }
+    result = this.param ? result : result.split("").reverse().join("");
+    return result;
   }
 }
 
